fb9ace00e8e14d292f3658e07ee06eba1aa8d375
Travis Vela <jtravisvela@gmail.com>
2025-06-14 00:17:30 -0500

    fix authUser issue

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useGetConversations = () => {
   const [loading, setLoading] = useState(false);
   const [conversations, setConversations] = useState([]);
+  const { authUser, setAuthUser } = useAuthContext("");
 
   useEffect(() => {
     const getConversations = async () => {
@@ -12,6 +14,7 @@ const useGetConversations = () => {
         const res = await fetch("/api/users", {});
         const data = await res.json();
         if (data.error) {
+          setAuthUser(null);
           throw new Error(data.error);
         }
         setConversations(data);
--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -22,7 +22,6 @@ const useLogin = () => {
       });
 
       const data = await res.json();
-      console.log(data);
 
       if (data.error) {
         throw new Error(data.error);
